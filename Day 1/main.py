cc612ed462b48595e87b5a46d6144dc18a0f51e3
Ahhj93 <69793084+Ahhj93@users.noreply.github.com>
2023-12-03 20:40:27 +0100

    AoC

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,5 +1,3 @@
-import re
-
 def problem1():
     input_text_file = open("input.txt", "r")
     input_list : list  = input_text_file.read().split("\n")
@@ -15,33 +13,48 @@ def problem1():
 
     print(somme_calibration)
 
-number : list = [None, "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-
 def problem2():
-    input_text_file = open("input.txt", "r")
+    input_text_file = open("test.txt", "r")
     input_list : list  = input_text_file.read().split("\n")
-    somme_calibration : int = 0
-    for item in input_list:
-        number_str : str = ""
-        number1 : list = [-1] * 10
-        number2 : list = [-1] * 10
-        for i in range (1,10):
-            n11 = re.search(rf'\b{number[i]}\b', item)
-            n12 = re.search(rf'\b{str(i)}\b', item)
-            if n11 and n12:
-                number1[i] = min([n11.start(), n12.start()])
-            if n11:
-                number1[i] = n11.start()
-            else:
-                number1[i] = n12.start()
-            number1[i] = min([re.search(rf'\b{number[i]}\b', item), re.search(rf'\b{str(i)}\b', item)])
-            number2[i] = max([item.rfind(number[i]), item.rfind(str(i))])
-        number_str += str(number1.index(min([i for i in number1 if i>=0])))
-        number_str += str(number2.index(max([i for i in number2 if i>=0])))
-        somme_calibration += int(number_str)
 
-    print(somme_calibration)
+    somme_calibration = 0
+    numbers_spelled = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9",
+    }
+
+    digits = "0123456789"
+
+    for item in input_list:
+        numbers = []
+        for value in digits:
+            index = item.find(value)
+            if index != -1:
+                numbers.append((index, value))
+            index = item.rfind(value)
+            if index != -1:
+                numbers.append((index, value))
+        for spelled, value in numbers_spelled.items():
+            index = item.find(spelled)
+            if index != -1:
+                numbers.append((index, value))
+            index = item.rfind(spelled)
+            if index != -1:
+                numbers.append((index, value))
+
+        numbers.sort()
+        somme_calibration += int(f"{numbers[0][1]}{numbers[-1][1]}")
+
+    return somme_calibration
 
 if __name__ == "__main__":
-    problem1()
-    problem2()
+    print("--- Day 1: --- Day 1: Trebuchet?! --- ---")
+    print(f"Part One: {problem1()}")
+    print(f"Part Two: {problem2()}")
--- Day 1/test.py
+++ Day 1/test.py
@@ -1,18 +1,3 @@
-import re
-
-number = "six"
-item = "7pqrstsixteen"
-
-# Recherche de l'indice pour number[i]
-match_number_i = re.search(rf'\b{number[1]}\b', item)
-index_number_i = match_number_i.start() if match_number_i else -1
-
-# Recherche de l'indice pour str(i)
-i = 1
-match_str_i = re.search(rf'\b{str(i)}\b', item)
-index_str_i = match_str_i.start() if match_str_i else -1
-
-# Récupération de l'indice minimum
-min_index = min(index_number_i, index_str_i)
-
-print(f"L'indice minimum est : {min_index}")
+a = "onea"
+index = a.find("one")
+print(index)
