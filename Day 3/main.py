f27b7ed631c12254d26e4601c724d6440da7cce7
Ahhj93 <69793084+Ahhj93@users.noreply.github.com>
2023-12-03 14:18:50 +0100

    AoC Day 3

--- Day 3/main.py
+++ Day 3/main.py
@@ -4,6 +4,9 @@ input_list : list  = input_text_file.read().splitlines()
 def is_symbol(char: str) -> bool:
     return not char == '.' and not char.isdigit()
 
+def is_symbol_star(char: str) -> bool:
+    return char == '*'
+
 def is_valid_index(i: int, j: int, rows: int, cols: int) -> bool:
     return 0 <= i < rows and 0 <= j < cols
 
@@ -53,6 +56,63 @@ def calculate_part_numbers(input_list : list) -> int:
 
     return part_numbers_sum
 
+def calculate_gear_part(input_list: list) -> int:
+    rows: int = len(input_list)
+    cols: int = len(input_list[0])
+    gear = {}
+
+    i: int = 0
+    while i < rows:
+        j: int = 0
+        while j < cols:
+            current_char = input_list[i][j]
+
+            if current_char.isdigit():
+                is_part_gear_number: bool = False
+
+                # Check all adjacent positions (including diagonals)
+                for x in range(-1, 2):
+                    for y in range(-1, 2):
+                        if is_valid_index(i + x, j + y, rows, cols) and is_symbol_star(input_list[i + x][j + y]):
+                            star_pos = (i + x, j + y)
+                            is_part_gear_number = True
+                            break
+
+                if is_part_gear_number:
+                    #Récupérer le nombre
+                    num1: bool = False
+                    begin_num: int = j
+                    while not num1 and begin_num >= 0:
+                        if input_list[i][begin_num].isdigit():
+                            begin_num -= 1
+                        else:
+                            num1 = True
+                    num2: bool = False
+                    end_num: int = j
+                    while not num2 and end_num < cols:
+                        if input_list[i][end_num].isdigit():
+                            end_num += 1
+                        else:
+                            num2 = True
+                    if input_list[i][begin_num + 1:end_num] != '':
+                        if star_pos not in gear:
+                            gear[star_pos] = []
+                        gear[star_pos].append(int(input_list[i][begin_num + 1:end_num]))
+                    j = end_num
+            j += 1
+        i += 1
+
+    sum_gear: int = 0
+    for key in gear:
+        if len(gear[key]) == 2:
+            gear_ratio: int = 1
+            for elt in gear[key]:
+                gear_ratio *= elt
+            sum_gear += gear_ratio
+
+    return sum_gear
+
 if __name__ == "__main__":
     print("--- Day 3: Gear Ratios ---")
     print(f"Part One: {calculate_part_numbers(input_list)}")
+    print(f"Part Two: {calculate_gear_part(input_list)}")
