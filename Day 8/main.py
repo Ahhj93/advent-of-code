a15d03ed9f2d05a67e901cf047b49988ec252b81
Ahhj93 <69793084+Ahhj93@users.noreply.github.com>
2023-12-08 20:50:08 +0100

    AoC Day 8

--- Day 8/main.py
+++ Day 8/main.py
@@ -1,17 +1,22 @@
-def parse_input(input):
-    input = input.split("\n\n")
-    instructions = (input[0].split("\n"))[0]
-    rules = input[1].split("\n")
-    nodes = {}
+def parse_input(input: str) -> (str, dict):
+    """
+    Parse the input into a list of instructions and a dictionary of nodes.
+    :param input: The input string.
+    :return: A tuple of the instructions and nodes.
+    """
+    input: str = input.split("\n\n")
+    instructions: str = (input[0].split("\n"))[0]
+    rules: str = input[1].split("\n")
+    nodes: dict = {}
     for rule in rules:
         rule = rule.split(" = ")
         nodes[rule[0]] = rule[1].replace("(", "").replace(")", "").split(", ")
     return instructions, nodes
 
-def to_zzz(instructions, nodes):
-    count = 0
-    i = 0
-    current = "AAA"
+def to_zzz(instructions: str, nodes: dict) -> int:
+    count: int = 0
+    i: int = 0
+    current: str = "AAA"
     while (current != "ZZZ"):
         if i == len(instructions):
             i = -1
@@ -26,9 +31,10 @@ def to_zzz(instructions, nodes):
 
 def main():
     input_text_file = open("input.txt", "r")
-    input_text = input_text_file.read()
+    input_text: str = input_text_file.read()
     instructions, nodes = parse_input(input_text)
     return to_zzz(instructions, nodes)
 
 if __name__ == "__main__":
-    print(main())
+    print("--- Day 8: Haunted Wasteland ---")
+    print(f"Part One: {main()}")
